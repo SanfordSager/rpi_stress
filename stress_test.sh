70ead0d6e0419f774eb243338ad3f7224c124ac8
flamboyantOne <40245703+flamboyantOne@users.noreply.github.com>
2018-08-22 18:54:39 -0700

    Just edited stress_test.sh so it opens a lot of chrome windows... I expect a crash.

--- stress_test.sh
+++ stress_test.sh
@@ -2,74 +2,118 @@ while true;
 do
 	cd /usr/share/applications/
 	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	chromium-browser
-	
 	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
+	/opt/vc/bin/vgencmd measure_temp
+	chromium-browser
 	/opt/vc/bin/vgencmd get_config arm_freq
 done
 
